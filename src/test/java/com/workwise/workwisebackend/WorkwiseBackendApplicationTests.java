a4ead62975c8a03f58f16f88648d264e79d594b2
Simone Chiarelli <simon.info.flair@gmail.com>
2024-07-01 01:00:16 +0200

    First Configuration: application.yml; external jar library; pom.xml; mavel dependencies

---
+++ src/test/java/com/workwise/workwisebackend/WorkwiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workwise.workwisebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkwiseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
