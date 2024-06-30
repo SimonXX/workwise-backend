4ec08676477c66545336c4709541b96ad4146219
Simone Chiarelli <simon.info.flair@gmail.com>
2024-07-01 01:00:16 +0200

    First Configuration: application.yml; external jar library; pom.xml; mavel dependencies

---
+++ src/main/java/com/workwise/workwisebackend/WorkwiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workwise.workwisebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkwiseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkwiseBackendApplication.class, args);
+    }
+
+}
